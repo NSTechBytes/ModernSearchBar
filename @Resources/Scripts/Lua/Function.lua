79b8448651d1a9ffda4c807fd936922f14a8a55c
Nasir Shahbaz <nstechbytes@gmail.com>
2024-12-17 20:47:34 +0500

    Releases Version 55.0
    1. Added the ChromeStatus Pluign for the realtime check of installation of chrome.
    2. Added the ModernSearchBar Plugin for the Chrome History and Top Trends and get rid from ps1 for faster and stable response.
    3. Added the BlurInput Plugin for the text input.
    4. Remove the System.Data.Sqlite.
    5. Decrease Size of Skin.

--- @Resources/Scripts/Lua/Function.lua
+++ @Resources/Scripts/Lua/Function.lua
@@ -10,3 +10,44 @@ function startslider(variant, handler, offsetx,offsety)
 	SKIN:Bang('!Move', PosX, PosY, 'ModernSearchBar\\Slider')
 end
 
+
+function trimAndProcess(input, type)
+    if string.find(input, "Not Internet") then
+		SKIN:Bang ('!HideMeterGroup', 'Trends')
+		SKIN:Bang ('!ShowMeterGroup', 'NotInternet')
+		SKIN:Bang ('!SetOption', 'recent_Image' ,'Y' , '(190*#Scale#)')
+		SKIN:Bang ('!UpdateMeter', '*')
+		SKIN:Bang ('!Redraw')
+       --- print("not internet")
+        return
+    end
+    if type == "TopTrends" then
+       -- print("Processing with type: " .. type)
+    else
+       -- print("Processing with another type: " .. type)
+    end
+
+   
+    local results = {}
+    for str in string.gmatch(input, "([^|]+)") do
+        table.insert(results, str)
+    end
+
+   
+    for i, str in ipairs(results) do
+        if type == "TopTrends" then
+        ---print("String " .. i .. ": " .. str)
+        SKIN:Bang ('!SetOption', 'Trends_Text'..i..'' , 'Text', str)
+		SKIN:Bang ('!SetVariable', 'Trends'..i..'' , str)
+        SKIN:Bang ('!UpdateMeter', 'Trends_Text'..i..'')
+        SKIN:Bang ('!Redraw')
+        else
+           -- print("String " .. i .. ": " .. str)
+            SKIN:Bang ('!SetOption', 'History_Text'..i..'' , 'Text', str)
+			SKIN:Bang ('!SetVariable', 'History'..i..'' , str)
+            SKIN:Bang ('!UpdateMeter', 'History_Text'..i..'')
+            SKIN:Bang ('!Redraw')
+        end
+
+    end
+end
